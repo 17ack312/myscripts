1e614fe3692a06e21e4da43fb3cdab0f4a297a16
Rajdeep basu <48894311+17ack312@users.noreply.github.com>
2022-01-22 03:57:44 +0530

    Update hex_decrypt.py

--- hex_decrypt.py
+++ hex_decrypt.py
@@ -1,11 +1,9 @@
 import requests,sys,os
 
-def decypt(text):
-    #url='https://online-toolz.com//functions/DECRYPT.php'
+def decrypt(text):
     url = 'https://online-toolz.com//functions/HEX-TEXT.php'
     payload="input="+text
     header={
-        #'Cookie':'PHPSESSID=680at9it9k24ptc30aga74l5r2; __gads=ID=9211ec0e3d182f6b-22585b831ad00025:T=1642797007:RT=1642797007:S=ALNI_MYchLzCDAnleLuXl9ijfm8CdUayiw; _ga_JY9C3TP5R4=GS1.1.1642797016.1.0.1642797016.0; _ga=GA1.2.1468353587.1642797008; _gid=GA1.2.1956302364.1642797019',
         'Content-Type':'application/x-www-form-urlencoded',
         'Referer': 'https://online-toolz.com/tools/text-encryption-decryption.php',
         'Accept': '*/*',
