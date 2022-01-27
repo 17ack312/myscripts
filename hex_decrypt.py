671b2ac57ca75fa23175e69d4c624323db879427
Rajdeep basu <48894311+17ack312@users.noreply.github.com>
2022-01-27 08:14:47 +0530

    Update hex_decrypt.py

--- hex_decrypt.py
+++ hex_decrypt.py
@@ -1,1 +1,1 @@
-import requests,sys,os;text='#$#$';url = 'https://online-toolz.com//functions/HEX-TEXT.php';payload="input="+text;header={'Content-Type':'application/x-www-form-urlencoded'};res = requests.post(url, data=payload ,headers=header);print(res.text)
+import requests,sys,os;text='#$#$';url = 'https://online-toolz.com//functions/HEX-TEXT.php';payload="input="+text;header={'Content-Type':'application/x-www-form-urlencoded'};res = requests.post(url, data=payload ,headers=header);res=(res.content.decode())
