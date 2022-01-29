dd2e6cbec4c3eee7b8d8355fa402ec94fdb71458
Rajdeep basu <48894311+17ack312@users.noreply.github.com>
2022-01-29 07:06:21 +0530

    Update hex_decrypt.py

--- hex_decrypt.py
+++ hex_decrypt.py
@@ -3,5 +3,5 @@ url = 'https://online-toolz.com//functions/HEX-TEXT.php'
 payload="input="+text
 header={'Content-Type':'application/x-www-form-urlencoded'}
 res = requests.post(url, data=payload ,headers=header)
-res=(res.content.decode())
+res=str(res.content.decode())
 #print(res)
